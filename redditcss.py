e2a40276a5ab7550e0c66afba610ca516e8c5be7
Robert Veitch <Robert@Fluxflashor.net>
2012-09-09 23:37:01 -0400

    added console output

--- redditcss.py
+++ redditcss.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
+# The most ghetto script you will probably ever find.
+# Reads a bunch of css files and replaces local image refs to
+# refs that will work on reddit. Requires code to be indented with
+# two spaces and the line must start with certain properties. I
+# really didn't want to bother with regex. Too annoying for this.
+# Written by Robert "Fluxflashor" Veitch
 import os
 
 FOLDERPATH = "css/"
-
-
-# Get list of all files in the folder
-
 
 # Open up each file one by one
 dirlist=os.listdir(FOLDERPATH)
@@ -15,21 +17,26 @@ try:
     for fname in dirlist:
         try:
             thefile = open(FOLDERPATH+fname, 'r')
-            print fname + " opened."
+            print fname + " opened"
         
             for line in thefile.readlines():
-                if(line.startswith('  background:') or line.startswith('  background-image:') or line.startswith('  content:')):
+                if line.startswith('  background:') or line.startswith('  background-image:') or line.startswith('  content:'):
+                    print "--\nMatched ("+fname+"):\nOld Line: " + line
+
                     line = line.replace("../images/", "%%")
                     line = line.replace(".png')", "%%')")
 
                     ##] Get rid more more annoying dev stuff
                     line = line.replace("sprites/", "")
                     line = line.replace("icons/", "")
+                    print "New Line: " + line
+
                     masterfile.write(line)
                 else:         
                     masterfile.write(line)
 
             thefile.close()
+            print fname + " closed."
         except IOError as e:
             print "Couldn't open file " + e
 
